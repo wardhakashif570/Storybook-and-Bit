b87ae8131737236ad18900078f8cf9aa1e4dcb13
Wardha Kashif <wardhakashif570@gmail.com>
2021-01-07 00:56:13 -0800

    storybook

---
+++ src/components/button.js
@@ -0,0 +1,12 @@
+  
+import React from 'react';
+
+const Button = ({ value, style, className, onClick }) => (
+    <button style={style}
+        className={className}
+        onClick={onClick}>
+        {value}
+    </button>
+)
+
+export default Button;
